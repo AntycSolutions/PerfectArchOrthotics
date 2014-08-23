8cd554c79a9004fbafbabe30d55e751a0cf7dd17
eklinger-UofA <eklinger@ualberta.ca>
2014-08-23 13:51:33 -0600

    Updated urls.py

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    #url(r'^$', 'views.home', name='home'),
+    url(r'^$', 'orthotics_project.views.index', name='index'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^clients/', include('clients.urls')),
+    url(r'^inventory/', include('inventory.urls')),
 )
