738b1b1fd199aea957bcc1ddf2de6e9e95718315
eklinger-UofA <eklinger@ualberta.ca>
2014-08-30 13:33:33 -0600

    Added user logout

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'views.index', name='index'),
     url(r'^login/', 'views.user_login', name='user_login'),
+    url(r'^logout/', 'views.user_logout', name='user_logout'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^clients/', include('clients.urls')),
     url(r'^inventory/', include('inventory.urls')),
--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from clients.models import Client
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+
+# Restricting access. User this decorator
+# @login_required
+# import from django.contrib.auth.decorators
+# Now to redirect the user, give them a way to get to the login url
+# in settings.py LOGIN_URL = '/login/'
 
 def index(request):
     context = RequestContext(request)
@@ -26,3 +32,8 @@ def user_login(request):
             return HttpResponse("Invalid login details supplied")
     else:
         return render_to_response('login.html', {}, context)
+
+
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/')
