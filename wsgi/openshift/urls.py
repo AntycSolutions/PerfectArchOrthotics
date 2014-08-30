6d82e881129b4d6d31eb001fd64c60f8589a57c9
eklinger-UofA <eklinger@ualberta.ca>
2014-08-30 13:27:28 -0600

    Added user login

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -6,8 +6,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    #url(r'^$', 'views.home', name='home'),
     url(r'^$', 'views.index', name='index'),
+    url(r'^login/', 'views.user_login', name='user_login'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^clients/', include('clients.urls')),
     url(r'^inventory/', include('inventory.urls')),
--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from clients.models import Client
+from django.contrib.auth import authenticate, login
 
 def index(request):
     context = RequestContext(request)
@@ -9,3 +10,19 @@ def index(request):
     return render_to_response('index.html', context)
 
 
+def user_login(request):
+    context = RequestContext(request)
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                return HttpResponse("Your account is deactivated")
+        else:
+            return HttpResponse("Invalid login details supplied")
+    else:
+        return render_to_response('login.html', {}, context)
