07e872e7186fd9b821d00abc24dda9b4ebd630db
eklinger-UofA <eklinger@ualberta.ca>
2014-09-17 18:41:50 -0600

    Added mathfilters and added reminaing coverage to insurance page

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mathfilters',
     'clients',
     'south',
     'easy_pdf',
