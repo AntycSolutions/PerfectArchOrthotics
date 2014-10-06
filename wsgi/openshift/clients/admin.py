e8672d85563429f92af3d37a45de642e3f17d5ff
eklinger-UofA <eklinger@ualberta.ca>
2014-10-05 20:16:49 -0600

    Added person model to admin page

--- wsgi/openshift/clients/admin.py
+++ wsgi/openshift/clients/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from clients.models import Client, Prescription, Insurance, Claim, Dependent, CoverageType, InsuranceClaim
+from clients.models import Client, Prescription, Insurance, Claim, Dependent, CoverageType, InsuranceClaim, Person
 
 admin.site.register(Client)
+admin.site.register(Person)
 admin.site.register(Prescription)
 admin.site.register(Insurance)
 admin.site.register(CoverageType)
