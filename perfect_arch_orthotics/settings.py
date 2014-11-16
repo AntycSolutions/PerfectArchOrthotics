f1dd431862320ee14472df3e8b79180666b9e877
Solanar <and_555@hotmail.com>
2014-11-16 15:48:29 -0700

    django 1.7 has migrations so deprecate south

--- perfect_arch_orthotics/settings.py
+++ perfect_arch_orthotics/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'south',
+    #'south',
     'easy_pdf',
     'mathfilters',
     'clients',
