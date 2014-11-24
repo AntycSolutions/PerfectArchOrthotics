ae56c5e470a69128f814f77a57332c52681bc9d9
Solanar <and_555@hotmail.com>
2014-11-23 21:38:17 -0700

    success url

--- clients/views/client.py
+++ clients/views/client.py
@@ -10,3 +10,9 @@ class DeleteClientView(DeleteView):
     slug_field = "id"
     slug_url_kwarg = "client_id"
     success_url = reverse_lazy('client_index')
+
+    def get_success_url(self):
+        client_id = self.object.id
+        self.success_url = reverse_lazy('client',
+                                        kwargs={'client_id': client_id})
+        return self.success_url
