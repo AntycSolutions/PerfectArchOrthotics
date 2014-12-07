92d33b686b26b5baa0df1e4bf885b34bf34fbfb3
Solanar <and_555@hotmail.com>
2014-12-07 13:18:40 -0700

    switch to meta forms based on models, create LaboratoryProofOfManufacturingFormSet and LaboratoryInsuranceLetterFormSet

--- clients/forms/forms.py
+++ clients/forms/forms.py
@@ -6,143 +6,133 @@ from clients.models import Client, Dependent, Insurance, CoverageType, Claim, \
 
 
 class ClientForm(forms.ModelForm):
-    MALE = 'Male'
-    FEMALE = 'Female'
-    GENDER_CHOICES = ((MALE, 'Male'),
-                      (FEMALE, 'Female'))
-
     # required
-    firstName = forms.CharField(max_length=128, help_text="Client first name")
-    lastName = forms.CharField(max_length=128, help_text="Client last name")
-    birthdate = forms.CharField(help_text="Client birthdate")
+    # first_name = forms.CharField(
+    #     max_length=128, help_text="Client first name")
+    # last_name = forms.CharField(
+    #     max_length=128, help_text="Client last name")
+    # birth_date = forms.CharField(
+    #     help_text="Client birthdate")
 
     # not required
-    address = forms.CharField(max_length=128,
-                              help_text="Client address", required=False)
-    city = forms.CharField(max_length=128,
-                           help_text="Client city", required=False)
-    postalCode = forms.CharField(max_length=6,
-                                 help_text="Client postal code (no spaces)",
-                                 required=False)
-    phoneNumber = forms.CharField(max_length=14,
-                                  help_text="Client home phone",
-                                  required=False)
-    cellNumber = forms.CharField(max_length=14,
-                                 help_text="Client cell phone", required=False)
-    email = forms.CharField(max_length=254,
-                            help_text="Client email", required=False)
-    gender = forms.ChoiceField(choices=GENDER_CHOICES,
-                               help_text="Gender", required=False)
-    employer = forms.CharField(max_length=128,
-                               help_text="Client employer", required=False)
-    referredBy = forms.CharField(max_length=128, required=False)
-    healthcareNumber = forms.CharField(max_length=20, required=False)
+    # address = forms.CharField(
+    #     max_length=128, help_text="Client address", required=False)
+    # city = forms.CharField(
+    #     max_length=128, help_text="Client city", required=False)
+    # postal_code = forms.CharField(
+    #     max_length=6, help_text="Client postal code (no spaces)",
+    #     required=False)
+    # phone_number = forms.CharField(
+    #     max_length=14, help_text="Client home phone", required=False)
+    # cell_number = forms.CharField(
+    #     max_length=14, help_text="Client cell phone", required=False)
+    # email = forms.CharField(
+    #     max_length=254, help_text="Client email", required=False)
+    # gender = forms.ChoiceField(
+    #     choices=Person.GENDER_CHOICES, help_text="Gender", required=False)
+    # employer = forms.CharField(
+    #     max_length=128, help_text="Client employer", required=False)
+    # referred_by = forms.CharField(
+    #     max_length=128, required=False)
+    # health_care_number = forms.CharField(
+    #     max_length=20, required=False)
 
     class Meta:
         model = Client
 
-        fields = ('firstName', 'lastName', 'birthdate', 'address', 'city',
-                  'postalCode', 'phoneNumber', 'cellNumber', 'email',
-                  'healthcareNumber', 'gender', 'employer', 'referredBy')
+        # fields = ('firstName', 'lastName', 'birthdate', 'address', 'city',
+        #           'postalCode', 'phoneNumber', 'cellNumber', 'email',
+        #           'healthcareNumber', 'gender', 'employer', 'referredBy')
+        exclude = ('dependents',)
 
-    def clean(self):
-        cleaned_data = super(ClientForm, self).clean()
-        if 'birthdate' in cleaned_data.keys():
-            if len(cleaned_data['birthdate'].split('/')) > 1:
-                m, d, y = cleaned_data['birthdate'].split('/')
-                new_birthdate = "%s-%s-%s" % (y, m, d)
-                cleaned_data['birthdate'] = new_birthdate
-        return cleaned_data
+    # def clean(self):
+    #     cleaned_data = super(ClientForm, self).clean()
+    #     if 'birth_date' in cleaned_data.keys():
+    #         if len(cleaned_data['birth_date'].split('/')) > 1:
+    #             m, d, y = cleaned_data['birth_date'].split('/')
+    #             new_birth_date = "%s-%s-%s" % (y, m, d)
+    #             cleaned_data['birth_date'] = new_birth_date
+    #     return cleaned_data
 
 
 class DependentForm(forms.ModelForm):
-    SPOUSE = 'Spouse'
-    CHILD = 'Child'
-    RELATIONSHIP_CHOICES = ((SPOUSE, 'Spouse'),
-                            (CHILD, 'Child'))
-
-    MALE = 'Male'
-    FEMALE = 'Female'
-    GENDER_CHOICES = ((MALE, 'Male'),
-                      (FEMALE, 'Female'))
-
-    firstName = forms.CharField(max_length=128,
-                                help_text="Dependent first name")
-    lastName = forms.CharField(max_length=128,
-                               help_text="Dependent last name")
-    birthdate = forms.CharField(help_text="Dependent birthdate")
-    gender = forms.ChoiceField(choices=GENDER_CHOICES,
-                               help_text="Gender", required=False)
-    relationship = forms.ChoiceField(choices=RELATIONSHIP_CHOICES,
-                                     help_text="Relationship", required=False)
+    # first_name = forms.CharField(
+    #     max_length=128, help_text="Dependent first name")
+    # last_name = forms.CharField(
+    #     max_length=128, help_text="Dependent last name")
+    # birth_date = forms.CharField(
+    #     help_text="Dependent birthdate")
+    # gender = forms.ChoiceField(
+    #     choices=Dependent.GENDER_CHOICES, help_text="Gender", required=False)
+    # relationship = forms.ChoiceField(
+    #     choices=Dependent.RELATIONSHIP_CHOICES, help_text="Relationship",
+    #     required=False)
 
     class Meta:
         model = Dependent
 
-        fields = ('firstName', 'lastName', 'birthdate', 'gender',
-                  'relationship')
+        # fields = ('firstName', 'lastName', 'birthdate', 'gender',
+        #           'relationship')
+        fields = ('__all__')
 
-    def clean(self):
-        cleaned_data = super(DependentForm, self).clean()
-        if 'birthdate' in cleaned_data.keys():
-            if len(cleaned_data['birthdate'].split('/')) > 1:
-                m, d, y = cleaned_data['birthdate'].split('/')
-                new_birthdate = "%s-%s-%s" % (y, m, d)
-                cleaned_data['birthdate'] = new_birthdate
-        return cleaned_data
+    # def clean(self):
+    #     cleaned_data = super(DependentForm, self).clean()
+    #     if 'birth_date' in cleaned_data.keys():
+    #         if len(cleaned_data['birth_date'].split('/')) > 1:
+    #             m, d, y = cleaned_data['birth_date'].split('/')
+    #             new_birth_date = "%s-%s-%s" % (y, m, d)
+    #             cleaned_data['birth_date'] = new_birth_date
+    #     return cleaned_data
 
 
 class InsuranceForm(forms.ModelForm):
-    BILLING_CHOICES = (("Direct", "Direct"),
-                       ("Indirect", "Indirect"))
-
-    provider = forms.CharField(max_length=128)
-    policyNumber = forms.CharField(max_length=128, required=False)
-    contractNumber = forms.CharField(max_length=128, required=False)
-    billing = forms.ChoiceField(choices=BILLING_CHOICES, required=False)
-    gaitScan = forms.BooleanField(required=False)
-    insuranceCard = forms.BooleanField(required=False)
+    # provider = forms.CharField(
+    #     max_length=128)
+    # policy_number = forms.CharField(
+    #     max_length=128, required=False)
+    # contract_number = forms.CharField(
+    #     max_length=128, required=False)
+    # billing = forms.ChoiceField(
+    #     choices=Insurance.BILLING_CHOICES, required=False)
+    # gait_scan = forms.BooleanField(
+    #     required=False)
+    # insurance_card = forms.BooleanField(
+    #     required=False)
 
     class Meta:
         model = Insurance
 
-        fields = ('provider', 'policyNumber', 'contractNumber', 'billing',
-                  'gaitScan', 'insuranceCard')
+        # fields = ('provider', 'policyNumber', 'contractNumber', 'billing',
+        #           'gaitScan', 'insuranceCard')
+        exclude = ('client', 'spouse')
 
 
 class CoverageForm(forms.ModelForm):
-    COVERAGE_TYPE = (("Orthotics", "Orthotics"),
-                     ("Compression_stockings", "Compression Stockings"),
-                     ("Orthopedic_shoes", "Orthopedic Shoes"))
-
-    coverageType = forms.ChoiceField(choices=COVERAGE_TYPE, required=False)
-    coveragePercent = forms.IntegerField()
-    maxClaimAmount = forms.IntegerField()
-    quantity = forms.IntegerField()
-    period = forms.IntegerField()
+    # coverage_type = forms.ChoiceField(
+    #     choices=CoverageType.COVERAGE_TYPE, required=False)
+    # coverage_percent = forms.IntegerField()
+    # max_claim_amount = forms.IntegerField()
+    # quantity = forms.IntegerField()
+    # period = forms.IntegerField()
 
     class Meta:
         model = CoverageType
 
-        fields = ('coverageType', 'coveragePercent', 'maxClaimAmount',
-                  'quantity', 'period')
+        # fields = ('coverageType', 'coveragePercent', 'maxClaimAmount',
+        #           'quantity', 'period')
+        exclude = ('insurance',)
 
 
 class ClaimForm(forms.ModelForm):
-    CLAIM_TYPE = (("Orthotics", "Orthotics"),
-                  ("Compression_stockings", "Compression Stockings"),
-                  ("Orthopedic_shoes", "Orthopedic Shoes"))
-    PAYMENT_CHOICES = (("CASH", "Cash"),
-                       ("CHEQUE", "Cheque"),
-                       ("CREDIT", "Credit"))
-
-    claimType = forms.ChoiceField(choices=CLAIM_TYPE, required=True)
-    paymentType = forms.ChoiceField(choices=PAYMENT_CHOICES, required=True)
+    # payment_type = forms.ChoiceField(
+    #     choices=Claim.PAYMENT_CHOICES, required=True)
 
     class Meta:
         model = Claim
 
-        fields = ('claimType', 'paymentType',)
+        # fields = ('claimType', 'paymentType',)
+        exclude = ('client', 'patient', 'coverage_types', 'amount_claimed',
+                   'expected_back')
 
 
 class InvoiceForm(forms.ModelForm):
@@ -166,8 +156,20 @@ class ProofOfManufacturingForm(forms.ModelForm):
         exclude = ('claim',)
 
 
-ItemFormSet = inlineformset_factory(Invoice, Item,
-                                    exclude=('invoice',))
+ItemFormSet = inlineformset_factory(
+    Invoice, Item,
+    exclude=('invoice',)
+)
 
-LaboratoryFormSet = inlineformset_factory(InsuranceLetter, Laboratory,
-                                          exclude=('insurance_letter',))
+LaboratoryInsuranceLetterFormSet = inlineformset_factory(
+    InsuranceLetter,
+    Laboratory,
+    exclude=('insurance_letter', 'proof_of_manufacturing')
+)
+
+LaboratoryProofOfManufacturingFormSet = inlineformset_factory(
+    ProofOfManufacturing,
+    Laboratory,
+    exclude=('insurance_letter', 'proof_of_manufacturing'),
+    extra=1
+)
