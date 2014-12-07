425a7b832c8e98a93d23f92107fe804e5150b883
Solanar <and_555@hotmail.com>
2014-12-07 13:04:59 -0700

    new settings practitioners laboratories bill_to ship_to, uncomment admins

--- perfect_arch_orthotics/configs/devl_settings.py
+++ perfect_arch_orthotics/configs/devl_settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # Get from file
-SECRET_KEY = ''
+SECRET_KEY = 'e#v=c7*5@x1qnp*tfke!f#+nd_amito34d%blwm)!8&@4u7=+y'
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -21,7 +21,18 @@ DATABASES = {
 }
 
 # Get from file
-# ADMINS = ()
+ADMINS = ()
+
+# Get from file
+PRACTITIONERS = ()
+
+# Get from file
+LABORATORIES = ()
+
+# Get from file
+BILL_TO = ()
+
+SHIP_TO = BILL_TO
 
 # Get from file
 # EMAIL_HOST_USER = ''
