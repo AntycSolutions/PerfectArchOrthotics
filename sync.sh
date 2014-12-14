612f88450eeee5a17c1beda92587a194a5211ca8
Solanar <and_555@hotmail.com>
2014-12-14 12:50:53 -0700

    load item fixture on sync

--- sync.sh
+++ sync.sh
@@ -2,5 +2,6 @@ rm db.sqlite3
 rm clients/migrations/0*
 python3 manage.py makemigrations
 python3 manage.py migrate
+python3 manage.py loaddata items.json
 python3 manage.py createsuperuser
 python3 populate.py
