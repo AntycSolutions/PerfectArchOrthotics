fd22f653dfd103c2405139709e35f9babdf1dd6d
Solanar <and_555@hotmail.com>
2014-12-16 01:46:28 -0700

    remove unneeded stats

--- clients/views/statistics.py
+++ clients/views/statistics.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
 
-from ..models import Client, Claim, SiteStatistics
+from ..models import SiteStatistics
 
 
 class Statistics(TemplateView):
@@ -8,14 +8,8 @@ class Statistics(TemplateView):
     context = dict()
 
     def get(self, request, *args, **kwargs):
-        #number of mechanics, number of inspection videos and views,
-        #number of sellers, etc.
-        clients_total = Client.objects.all().count()
-        claims_total = Claim.objects.all().count()
         statistics = SiteStatistics.objects.get_or_create(pk=0)[0]
 
-        self.context['clients_total'] = clients_total
-        self.context['claims_total'] = claims_total
         self.context['statistics'] = statistics
 
         return self.render_to_response(self.context)
