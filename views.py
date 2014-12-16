d530ef625bcfb3fc0139df128027a820305340f0
Solanar <and_555@hotmail.com>
2014-12-16 01:39:02 -0700

    remove home page views incrementer, remove old login view

--- views.py
+++ views.py
@@ -1,36 +1,13 @@
 from django.shortcuts import render_to_response
-from django.http import HttpResponseRedirect  # , HttpResponse
+from django.http import HttpResponseRedirect
 from django.template import RequestContext
-from django.contrib.auth import logout  # , authenticate, login,
+from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-
-from clients.models import SiteStatistics
 
 
 def index(request):
     context = RequestContext(request)
-    statistics = SiteStatistics.objects.get_or_create(pk=0)[0]
-    statistics.home_page_views += 1
-    statistics.save()
     return render_to_response('index.html', context)
-
-
-# def user_login(request):
-#     context = RequestContext(request)
-#     if request.method == "POST":
-#         username = request.POST['username']
-#         password = request.POST['password']
-#         user = authenticate(username=username, password=password)
-#         if user:
-#             if user.is_active:
-#                 login(request, user)
-#                 return HttpResponseRedirect('/')
-#             else:
-#                 return HttpResponse("Your account is inactive.")
-#         else:
-#             return HttpResponse("Invalid login, please try again.")
-#     else:
-#         return render_to_response('login.html', {}, context)
 
 
 @login_required
