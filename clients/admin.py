d584fc0c84d050853918be605dbcefd4792caaf3
Solanar <and_555@hotmail.com>
2014-12-19 01:46:54 -0700

    show claimcoveragetype in claim

--- clients/admin.py
+++ clients/admin.py
@@ -3,20 +3,34 @@ from django.contrib.auth.models import Group
 
 from clients.models import Client, Insurance, Claim, Dependent, \
     CoverageType, Person, Invoice, Item, InsuranceLetter, \
-    Laboratory, ProofOfManufacturing, ClaimItem
+    Laboratory, ProofOfManufacturing, ClaimItem, ClaimCoverageType
 
 admin.site.register(Client)
 admin.site.register(Person)
 admin.site.register(Insurance)
 admin.site.register(CoverageType)
-admin.site.register(Claim)
+
+
+class ClaimItemInline(admin.TabularInline):
+    model = ClaimItem
+
+
+class ClaimCoverageTypeInline(admin.TabularInline):
+    model = ClaimCoverageType
+
+
+class ClaimAdmin(admin.ModelAdmin):
+    inlines = (ClaimCoverageTypeInline, ClaimItemInline)
+admin.site.register(Claim, ClaimAdmin)
+
+admin.site.register(Item)
+admin.site.register(Laboratory)
 admin.site.register(Dependent)
 admin.site.register(Invoice)
-admin.site.register(Item)
 admin.site.register(InsuranceLetter)
-admin.site.register(Laboratory)
 admin.site.register(ProofOfManufacturing)
 admin.site.register(ClaimItem)
+admin.site.register(ClaimCoverageType)
 
 # Unused, dont display
 admin.site.unregister(Group)
