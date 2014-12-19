ac44bef023dcbdc985dffb019ac0711dbf2cfd48
Solanar <and_555@hotmail.com>
2014-12-19 01:45:43 -0700

    make cmd with user input last

--- sync.sh
+++ sync.sh
@@ -3,5 +3,5 @@ rm clients/migrations/0*
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py loaddata items.json
-python3 manage.py createsuperuser
 python3 populate.py
+python3 manage.py createsuperuser
