d3d7e720f603f4e8c3b31d0d6bced931ddd8cae5
Solanar <and_555@hotmail.com>
2014-12-19 01:47:43 -0700

    start of cbv for client

--- clients/views/client.py
+++ clients/views/client.py
@@ -1,7 +1,20 @@
 from django.core.urlresolvers import reverse_lazy
-from django.views.generic.edit import DeleteView
+from django.views.generic.edit import CreateView, DeleteView
 
 from clients.models import Client
+from clients.forms.forms import ClientForm
+
+
+class CreateClientView(CreateView):
+    template_name = 'clients/client/create_client.html'
+    model = Client
+    form_class = ClientForm
+
+    def get_success_url(self):
+        client_id = self.object.id
+        self.success_url = reverse_lazy('client',
+                                        kwargs={'client_id': client_id})
+        return self.success_url
 
 
 class DeleteClientView(DeleteView):
