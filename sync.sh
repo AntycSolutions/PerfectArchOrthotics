1b1688a89601fd9476f34c2270caf1cdc0baf7b0
Solanar <and_555@hotmail.com>
2014-12-23 00:06:35 -0700

    admin is in populate

--- sync.sh
+++ sync.sh
@@ -4,4 +4,4 @@ python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py loaddata items.json
 python3 populate.py
-python3 manage.py createsuperuser
+#python3 manage.py createsuperuser
