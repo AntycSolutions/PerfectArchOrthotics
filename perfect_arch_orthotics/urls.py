815da2869c713db31527fd6ca834f7d1877aea66
Solanar <and_555@hotmail.com>
2015-01-23 12:19:17 -0700

    add media urls

--- perfect_arch_orthotics/urls.py
+++ perfect_arch_orthotics/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
 from django.contrib import admin
 admin.autodiscover()
@@ -15,4 +17,4 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^clients/', include('clients.urls')),
     url(r'^inventory/', include('inventory.urls')),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
