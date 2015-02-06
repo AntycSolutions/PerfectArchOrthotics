1f76a394ab95e1d9b5b7f8749098df4cdba197e3
Solanar <and_555@hotmail.com>
2015-02-06 16:05:21 -0700

    add debug templatetag get_dict to return an objects __dict__

--- utils/templatetags/debug.py
+++ utils/templatetags/debug.py
@@ -10,3 +10,8 @@ def get_attr(model_instance, attr_name):
 @register.simple_tag
 def get_dir(model_instance):
     return dir(model_instance)
+
+
+@register.simple_tag
+def get_dict(model_instance):
+    return model_instance.__dict__
