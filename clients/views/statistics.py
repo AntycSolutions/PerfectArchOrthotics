7d2b921873d830124b36753f94859d0d7f9ecaf4
Solanar <and_555@hotmail.com>
2015-03-26 13:14:00 -0600

    add top 10 best selling shoes to statistics, add old ordered/arrived date orders to statistics, switch from get to get_context_data in statistics

--- clients/views/statistics.py
+++ clients/views/statistics.py
@@ -1,15 +1,41 @@
-from django.views.generic import TemplateView
+from datetime import datetime, timedelta
 
-from ..models import SiteStatistics
+from django.views.generic import TemplateView
+from django.db.models import Count
+
+from clients import models as clients_models
+from inventory import models as inventory_models
 
 
 class Statistics(TemplateView):
     template_name = "clients/statistics.html"
-    context = dict()
 
-    def get(self, request, *args, **kwargs):
-        statistics = SiteStatistics.objects.get_or_create(pk=0)[0]
+    def get_context_data(self, **kwargs):
+        context = super(Statistics, self).get_context_data(**kwargs)
 
-        self.context['statistics'] = statistics
+        statistics = clients_models.SiteStatistics.objects.get_or_create(
+            pk=0
+        )[0]
+        context['statistics'] = statistics
 
-        return self.render_to_response(self.context)
+        # Top 10 best sellers
+        shoes = inventory_models.Shoe.objects.annotate(
+            num_orders=Count('shoeattributes__shoeorder')
+        ).order_by('-num_orders')[:10]
+        context['shoes'] = shoes
+
+        cutoff = datetime.now() - timedelta(days=30)
+        orders = inventory_models.Order.objects.filter(
+            dispensed_date__isnull=True,
+            arrived_date__isnull=True,
+            ordered_date__lte=cutoff,
+        )
+        context['old_ordered_date_orders'] = orders
+
+        orders = inventory_models.Order.objects.filter(
+            dispensed_date__isnull=True,
+            arrived_date__lte=cutoff,
+        )
+        context['old_arrived_date_orders'] = orders
+
+        return context
