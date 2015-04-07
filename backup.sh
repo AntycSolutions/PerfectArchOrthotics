3129208f8972fd5edb17072face9726bb0a1f181
Solanar <and_555@hotmail.com>
2015-04-07 17:07:34 -0600

    backup script

---
+++ backup.sh
@@ -0,0 +1,22 @@
+# Backup database/media to a json file, tar/gzip the json file
+
+filename="default"
+now=$(date +"%Y_%m_%d")
+
+if [ -e "../devl" ]
+then
+    filename="devl"
+elif [ -e "../test"]
+then
+    filename="test"
+elif [ -e "../prod"]
+then
+    filename="prod"
+fi
+
+idx="$filename"_"$now"
+
+python3 manage.py dumpdata > "$idx".json
+tar -czf ../backups/"$idx".tar.gz "$idx".json
+tar -czf ../backups/media_"$idx".tar.gz "media/"
+rm "$idx".json
