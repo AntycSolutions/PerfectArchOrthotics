1f57062c417ec2505d97ed3f02ef9d000e43be89
Solanar <and_555@hotmail.com>
2015-04-24 12:31:57 -0600

    update BaseNestedFormSet to support files/media, simplify nestedformset_factory, update from github project, add MinimumInlineFormSet/MinimumNestedFormSet, add minimum_nestedformset_factory, formatting

--- clients/forms/forms.py
+++ clients/forms/forms.py
@@ -301,7 +301,6 @@ LaboratoryInsuranceLetterFormSet = inlineformset_factory(
 )
 
 
-# Needs to be updated for files/media
 class BaseNestedFormSet(BaseInlineFormSet):
 
     def add_fields(self, form, index):
@@ -309,9 +308,12 @@ class BaseNestedFormSet(BaseInlineFormSet):
 
         form.nested = self.nested_formset_class(
             instance=form.instance,
-            data=form.data if self.is_bound else None,
-            prefix='%s-%s' % (form.prefix,
-                              self.nested_formset_class.get_default_prefix())
+            data=form.data if form.is_bound else None,
+            files=form.files if form.is_bound else None,
+            prefix='%s-%s' % (
+                form.prefix,
+                self.nested_formset_class.get_default_prefix()
+            ),
         )
 
     def is_valid(self):
@@ -319,7 +321,8 @@ class BaseNestedFormSet(BaseInlineFormSet):
 
         if self.is_bound:
             for form in self.forms:
-                result = result and form.nested.is_valid()
+                if not self._should_delete_form(form):
+                    result = result and form.nested.is_valid()
 
         return result
 
@@ -339,6 +342,10 @@ class BaseNestedFormSet(BaseInlineFormSet):
 
         return result_tuples
 
+    @property
+    def media(self):
+        return self.empty_form.media + self.empty_form.nested.media
+
 
 class BaseNestedModelForm(forms.ModelForm):
 
@@ -346,32 +353,118 @@ class BaseNestedModelForm(forms.ModelForm):
         return (super(BaseNestedModelForm, self).has_changed()
                 or self.nested.has_changed())
 
-# class CoverageFormSet(BaseNestedFormSet):
 
-#     def __init__(self, *args, **kwargs):
-#         super(CoverageFormSet, self).__init__(*args, **kwargs)
+class MinimumInlineFormSet(BaseInlineFormSet):
+
+    def __init__(self, *args, **kwargs):
+        super(MinimumInlineFormSet, self).__init__(*args, **kwargs)
+
+        if not hasattr(self, 'minimum'):
+            self.minimum = 1
+        if not hasattr(self, 'minimum_name'):
+            self.minimum_name = self.model.__name__
+
+    def clean(self):
+        super(MinimumInlineFormSet, self).clean()
+
+        count = 0
+        for form in self.forms:
+            try:
+                if (form.cleaned_data
+                        and not form.cleaned_data.get('DELETE', False)):
+                    count += 1
+            except AttributeError:
+                # Invalid subform raises AttributeError for cleaned_data
+                pass
+
+        if (count < self.minimum) and self.validate_minimum:
+            raise forms.ValidationError(
+                'Please add at least %s %s%s.' % (
+                    self.minimum, self.minimum_name,
+                    '' if self.minimum == 1 else 's'
+                )
+            )
 
 
-# Needs to be updated to deal with nestednested
-def nestedformset_factory(parent_model, child_model, grandchild_model,
-                          **kwargs):
-    nested_kwargs = {}
-    for key in kwargs:
-        if ("nested_" in key):
-            nested_kwargs[key.replace("nested_", '')] = kwargs[key]
-    for key in nested_kwargs:
-        kwargs.pop("nested_" + key)
-    if 'formset' in kwargs:
-        formset = kwargs.pop('formset')
-    else:
-        formset = BaseNestedFormSet
-    if 'form' in kwargs:
-        form = kwargs.pop('form')
-    else:
-        form = BaseNestedModelForm
-    parent_child = inlineformset_factory(
-        parent_model, child_model, form=form, formset=formset, **kwargs)
-    parent_child.nested_formset_class = inlineformset_factory(
-        child_model, grandchild_model, **nested_kwargs)
+class MinimumNestedFormSet(BaseNestedFormSet):
 
-    return parent_child  # Is class, not instance
+    def __init__(self, *args, **kwargs):
+        super(MinimumNestedFormSet, self).__init__(*args, **kwargs)
+
+        if not hasattr(self, 'minimum'):
+            self.minimum = 1
+        if not hasattr(self, 'minimum_name'):
+            self.minimum_name = self.model.__name__
+
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            try:
+                form.nested.validate_minimum = False
+                if (form.cleaned_data
+                        and not form.cleaned_data.get('DELETE', False)):
+                    count += 1
+                    if form.cleaned_data['id'] or form.has_changed():
+                        form.nested.validate_minimum = True
+            except AttributeError:
+                # Invalid subform raises AttributeError for cleaned_data
+                pass
+
+        if count < self.minimum:
+            raise forms.ValidationError(
+                'Please add at least %s %s%s.' % (
+                    self.minimum, self.minimum_name,
+                    '' if self.minimum == 1 else 's'
+                )
+            )
+
+        super(MinimumNestedFormSet, self).clean()
+
+
+def nestedformset_factory(parent_model, child_model, nested_formset, **kwargs):
+    if 'formset' not in kwargs:
+        kwargs['formset'] = BaseNestedFormSet
+    if 'form' not in kwargs:
+        kwargs['form'] = BaseNestedModelForm
+
+    NestedFormSet = inlineformset_factory(
+        parent_model,
+        child_model,
+        **kwargs
+    )
+    NestedFormSet.nested_formset_class = nested_formset
+
+    return NestedFormSet  # Is class, not instance
+
+
+def minimum_nestedformset_factory(
+        parent_model, model, nested_formset,
+        minimum=1, minimum_name=None,
+        minimum_nested=1, minimum_name_nested=None,
+        **kwargs):
+    if 'formset' not in kwargs:
+        kwargs['formset'] = MinimumNestedFormSet
+    if 'form' not in kwargs:
+        kwargs['form'] = BaseNestedModelForm
+
+    NestedFormSet = inlineformset_factory(
+        parent_model,
+        model,
+        **kwargs
+    )
+    NestedFormSet.nested_formset_class = nested_formset
+
+    if not hasattr(NestedFormSet, 'minimum'):
+        NestedFormSet.minimum = minimum
+    if not hasattr(NestedFormSet, 'minimum_name'):
+        NestedFormSet.minimum_name = minimum_name
+    if not hasattr(NestedFormSet.nested_formset_class, 'minimum'):
+        NestedFormSet.nested_formset_class.minimum = (
+            minimum_nested or model.__name__
+        )
+    if not hasattr(NestedFormSet.nested_formset_class, 'minimum_name'):
+        NestedFormSet.nested_formset_class.minimum_name = (
+            minimum_name_nested or nested_formset.model.__name__
+        )
+
+    return NestedFormSet  # Is class, not instance
