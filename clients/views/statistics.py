b4e6efb3dc4585157b8200e1e3373962ef0d9bb9
Solanar <and_555@hotmail.com>
2015-05-06 01:24:11 -0600

    more stats, formatting, remove unneeded Q obj

--- clients/views/statistics.py
+++ clients/views/statistics.py
@@ -40,7 +40,9 @@ class Statistics(TemplateView):
         return context
 
     def _stats(self):
-        revenue_claims = clients_models.Claim.objects.annotate(
+        revenue_claims = clients_models.Claim.objects.select_related(
+            'insurance',
+        ).annotate(
             invoice_revenue=(
                 Coalesce(F('invoice__payment_made'), 0)
                 + Coalesce(F('invoice__deposit'), 0)
@@ -71,7 +73,8 @@ class Statistics(TemplateView):
         invoice_revenue = 0
         expected_back_revenue = 0
         outstanding_fees = 0
-        outstanding_clients = set()
+        outstanding_assignment_clients_set = set()
+        outstanding_non_assignment_clients_set = set()
         claims = zip(revenue_claims, outstanding_claims)
         for revenue_claim, outstanding_claim in claims:
             amount_remaining = 0
@@ -80,19 +83,38 @@ class Statistics(TemplateView):
                     outstanding_claim.total_amount
                     - revenue_claim.total_revenue
                 )
+                if revenue_claim.insurance.benefits is 'na':
+                    outstanding_non_assignment_clients_set.add(
+                        revenue_claim.patient_id
+                    )
+                elif revenue_claim.insurance.benefits is 'a':
+                    outstanding_assignment_clients_set.add(
+                        revenue_claim.patient_id
+                    )
             invoice_revenue += revenue_claim.invoice_revenue
             expected_back_revenue += revenue_claim.expected_back_revenue
             outstanding_fees += amount_remaining
-            outstanding_clients.add(revenue_claim.patient_id)
 
         total_revenue = invoice_revenue + expected_back_revenue
+        outstanding_assignment_clients = len(
+            outstanding_assignment_clients_set
+        )
+        outstanding_non_assignment_clients = len(
+            outstanding_non_assignment_clients_set
+        )
         stats_dict = {
             'invoice_revenue': invoice_revenue,
             'expected_back_revenue': expected_back_revenue,
             'outstanding_fees': outstanding_fees,
             'total_revenue': total_revenue,
             'total': total_revenue + outstanding_fees,
-            'outstanding_clients': len(outstanding_clients)
+            'outstanding_assignment_clients': outstanding_assignment_clients,
+            'outstanding_non_assignment_clients':
+                outstanding_non_assignment_clients,
+            'total_outstanding_clients': (
+                outstanding_assignment_clients
+                + outstanding_non_assignment_clients
+            ),
         }
 
         return stats_dict
@@ -106,15 +128,13 @@ class Statistics(TemplateView):
         insurances_expected_back = date_queryset.values(
             'provider',
         ).annotate(
-            non_assignment_expected_back=Sum(
-                Case(
-                    When(
-                        Q(benefits='na'),
-                        then='claim__claimcoverage__expected_back',
-                    ),
-                    default=0,
-                )
-            ),
+            non_assignment_expected_back=Sum(Case(
+                When(
+                    benefits='na',
+                    then='claim__claimcoverage__expected_back',
+                ),
+                default=0,
+            )),
             assignment_expected_back=Sum(Case(
                 When(
                     Q(benefits='a')
@@ -143,6 +163,7 @@ class Statistics(TemplateView):
                 + F('total_assignment_expected_back')
             )
         )
+
         # Total amount claimed
         date_queryset = views._date_search(
             self.request, ["claim__submitted_datetime"],
@@ -151,12 +172,39 @@ class Statistics(TemplateView):
         insurances_amount_claimed = date_queryset.values(
             'provider'
         ).annotate(
-            amount_claimed__sum=Sum(
-                Coalesce(
-                    F('claim__claimcoverage__claimitem__item__unit_price'), 0
-                )
-                * Coalesce(F('claim__claimcoverage__claimitem__quantity'), 0)
-            ),
+            assignment_amount_claimed=Sum(Case(
+                When(
+                    benefits='a',
+                    then=Coalesce(
+                        F('claim__claimcoverage__claimitem__item__unit_price'),
+                        0
+                    )
+                    * Coalesce(
+                        F('claim__claimcoverage__claimitem__quantity'),
+                        0
+                    )
+                ),
+                default=0,
+            )),
+            non_assignment_amount_claimed=Sum(Case(
+                When(
+                    benefits='na',
+                    then=Coalesce(
+                        F('claim__claimcoverage__claimitem__item__unit_price'),
+                        0
+                    )
+                    * Coalesce(
+                        F('claim__claimcoverage__claimitem__quantity'),
+                        0
+                    )
+                ),
+                default=0,
+            )),
+        ).annotate(
+            amount_claimed__sum=(
+                F('assignment_amount_claimed')
+                + F('non_assignment_amount_claimed')
+            )
         )
         # Combine the 2 above query's results into one
         insurances_chain = chain(
@@ -174,6 +222,8 @@ class Statistics(TemplateView):
             'pending_assignment_expected_back': 0,
             'total_assignment_expected_back': 0,
             'expected_back__sum': 0,
+            'non_assignment_amount_claimed': 0,
+            'assignment_amount_claimed': 0,
             'amount_claimed__sum': 0
         }
         for insurance in insurances:
@@ -182,6 +232,16 @@ class Statistics(TemplateView):
                     (insurance['num_claims'] or 0)
             else:
                 insurance['num_claims'] = 0
+            if 'non_assignment_amount_claimed' in insurance:
+                insurances_totals['non_assignment_amount_claimed'] += \
+                    (insurance['non_assignment_amount_claimed'] or 0)
+            else:
+                insurance['non_assignment_amount_claimed'] = 0
+            if 'assignment_amount_claimed' in insurance:
+                insurances_totals['assignment_amount_claimed'] += \
+                    (insurance['assignment_amount_claimed'] or 0)
+            else:
+                insurance['assignment_amount_claimed'] = 0
             if 'amount_claimed__sum' in insurance:
                 insurances_totals['amount_claimed__sum'] += \
                     (insurance['amount_claimed__sum'] or 0)
