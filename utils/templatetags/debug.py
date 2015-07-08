6037fa7b3f7d2c42f72cc3a3ef2deeb5b61b6225
Solanar <and_555@hotmail.com>
2015-07-07 21:50:31 -0600

    add sql_queries length to navbar if superuser

--- utils/templatetags/debug.py
+++ utils/templatetags/debug.py
@@ -15,3 +15,9 @@ def get_dir(model_instance):
 @register.simple_tag
 def get_dict(model_instance):
     return model_instance.__dict__
+
+
+@register.assignment_tag
+def sql_queries():
+    from django.db import connection
+    return connection.queries
