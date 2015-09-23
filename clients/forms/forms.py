9bb78d1c442542ff5d0b4d615d3aa6e5721977a6
Solanar <and_555@hotmail.com>
2015-09-23 13:51:56 -0600

    cleanup, add ReferralForm

--- clients/forms/forms.py
+++ clients/forms/forms.py
@@ -1,11 +1,10 @@
-from collections import OrderedDict
 from itertools import zip_longest
 import itertools
 
 from django import forms
-from django.forms import models, BaseInlineFormSet
-from django.forms import fields
+from django.forms import BaseInlineFormSet
 from django.forms.models import inlineformset_factory
+from django.template import defaultfilters
 
 from bootstrap3_datetime.widgets import DateTimePicker
 
@@ -14,6 +13,7 @@ from utils.forms import fields as utils_fields, widgets as utils_widgets
 from clients.models import Client, Dependent, Insurance, Coverage, Claim, \
     Invoice, InsuranceLetter, Laboratory, ProofOfManufacturing, Person, Item, \
     ClaimItem, ClaimCoverage, ClaimAttachment
+from clients import models as clients_models
 
 
 class ClientForm(forms.ModelForm):
@@ -22,30 +22,12 @@ class ClientForm(forms.ModelForm):
         model = Client
         fields = '__all__'
 
-    # def clean(self):
-    #     cleaned_data = super(ClientForm, self).clean()
-    #     if 'birth_date' in cleaned_data.keys():
-    #         if len(cleaned_data['birth_date'].split('/')) > 1:
-    #             m, d, y = cleaned_data['birth_date'].split('/')
-    #             new_birth_date = "%s-%s-%s" % (y, m, d)
-    #             cleaned_data['birth_date'] = new_birth_date
-    #     return cleaned_data
-
 
 class DependentForm(forms.ModelForm):
 
     class Meta:
         model = Dependent
         exclude = ('client',)
-
-    # def clean(self):
-    #     cleaned_data = super(DependentForm, self).clean()
-    #     if 'birth_date' in cleaned_data.keys():
-    #         if len(cleaned_data['birth_date'].split('/')) > 1:
-    #             m, d, y = cleaned_data['birth_date'].split('/')
-    #             new_birth_date = "%s-%s-%s" % (y, m, d)
-    #             cleaned_data['birth_date'] = new_birth_date
-    #     return cleaned_data
 
 
 class InsuranceForm(forms.ModelForm):
@@ -72,41 +54,6 @@ class CoverageForm(forms.ModelForm):
         exclude = ('insurance',)
 
 
-class CustomListModelChoiceIterator(models.ModelChoiceIterator):
-
-    def choice(self, obj):
-        return [self.field.prepare_value(obj),
-                self.field.label_from_instance(obj),
-                obj
-                ]
-
-
-class CustomDictModelChoiceIterator(models.ModelChoiceIterator):
-
-    def choice(self, obj):
-        return {'value': self.field.prepare_value(obj),
-                'label': self.field.label_from_instance(obj),
-                'obj': obj}
-
-
-class CustomModelMultipleChoiceField(forms.ModelMultipleChoiceField):
-
-    def _get_choices(self):
-        if hasattr(self, '_choices'):
-            return self._choices
-        return CustomListModelChoiceIterator(self)
-
-    def _get_choices_dict(self):
-        if hasattr(self, '_choices'):
-            return self._choices
-        return CustomDictModelChoiceIterator(self)
-
-    choices = property(_get_choices,
-                       fields.ChoiceField._set_choices)
-    choices_dict = property(_get_choices_dict,
-                            fields.ChoiceField._set_choices)
-
-
 class ClaimForm(forms.ModelForm):
     submitted_datetime = forms.DateTimeField(
         label="Submitted Datetime",
@@ -118,13 +65,6 @@ class ClaimForm(forms.ModelForm):
                    "style": "text-transform: uppercase; float: none;"},
         )
     )
-    # items = CustomModelMultipleChoiceField(
-    #     required=False,
-    #     queryset=Item.objects.all(),
-    #     widget=forms.CheckboxSelectMultiple)
-    # coverage_types = CustomModelMultipleChoiceField(
-    #     queryset=Coverage.objects.all(),
-    #     widget=forms.CheckboxSelectMultiple)
     claim_package = utils_fields.MultiFileField(
         label="Claim Package",
         required=False,
@@ -149,12 +89,7 @@ class ClaimForm(forms.ModelForm):
         self.initial['claim_package'] = file_list
 
         self.client = client
-    #     fields_order = ['patient', 'insurance', 'coverage_types', 'items']
-    #     if (len(fields_order) != len(self.fields)):
-    #         raise forms.ValidationError("Please contact the administrator"
-    #                                     ", ClaimForm is missing fields.")
-    #     self.fields = OrderedDict(
-    #         (name, self.fields[name]) for name in fields_order)
+
         patients = []
         spouse = None
         for dependent in client.dependent_set.all():
@@ -207,113 +142,6 @@ class ClaimForm(forms.ModelForm):
                 raise Exception("Unknown Exception during MultiFile save.")
 
         return instance
-
-    #     coverage_types = self.fields['coverage_types']
-    #     coverage_types.queryset = Coverage.objects.filter(
-    #         insurance__in=insurance.queryset).order_by('insurance')
-
-    #     items = self.fields['items']
-    #     items.queryset = Item.objects.filter(
-    #         coverage_type__in=coverage_types.queryset.values_list(
-    #             'coverage_type', flat=True).distinct()).order_by(
-    #                 'coverage_type', 'gender', 'product_code')
-
-    # def clean(self):
-    #     cleaned_data = super(ClaimForm, self).clean()
-    #     self.instance.client = self.client
-    #     items = cleaned_data.get("items")
-    #     coverage_types = cleaned_data.get("coverage_types")
-    #     if not coverage_types:
-    #         coverage_types = []
-
-    #     # valid = True
-    #     self.claim_coverage_types = []
-    #     self.claim_items = []
-    #     for coverage_type in coverage_types:
-    #         amount_claimed_total = 0
-    #         expected_back_total = 0
-    #         if items:
-    #             for item in items.filter(
-    #                     coverage_type=coverage_type.coverage_type):
-    #                 quantity = int(
-    #                     self.data['pairs_%s' % item.id])
-    #                 amount = item.unit_price
-    #                 amount_total = amount * quantity
-    #                 # coverage_remaining = coverage_type.coverage_remaining()
-    #                 # if quantity > coverage_type.quantity:
-    #                 #     valid = False
-    #                 #     self.add_error(
-    #                 #         "items",
-    #                 #         forms.ValidationError(
-    #                 #             "Product Code: "
-    #                 #             + self.data['product_code_%s' % item.id]
-    #                 #             + " Quantity is more than "
-    #                 #             + coverage_type.get_coverage_type_display()
-    #                 #             + " Pair Remaining."))
-    #                 # elif amount_total > coverage_remaining:
-    #                 #     valid = False
-    #                 #     self.add_error(
-    #                 #         "items",
-    #                 #         forms.ValidationError(
-    #                 #             "Product Code: "
-    #                 #             + self.data['product_code_%s' % item.id]
-    #                 #             + " Claim Amount * Quantity is more than "
-    #                 #             + coverage_type.get_coverage_type_display()
-    #                 #             + " Coverage Remaining"))
-    #                 # else:
-    #                 # coverage_type.quantity -= quantity
-    #                 # if coverage_type.quantity < 0:
-    #                 #     coverage_type.quantity = 0
-
-    #                 # coverage_type.total_claimed += actual_amount_claimed
-
-    #                 coverage_percent = coverage_type.coverage_percent
-    #                 amount_claimed_total += amount_total
-    #                 expected_back_total += (
-    #                     amount_total * (coverage_percent / 100))
-    #                 self.claim_items.append(
-    #                     ClaimItem(item=item, quantity=quantity)
-    #                 )
-    #         self.claim_coverage_types.append(
-    #             ClaimCoverage(
-    #                 coverage_type=coverage_type,
-    #                 amount_claimed=amount_claimed_total,
-    #                 expected_back=expected_back_total,
-    #             )
-    #         )
-
-    #     # if valid:
-    #     # for coverage_type in coverage_types:
-    #     #     coverage_type.save()
-
-    # def save(self, commit=True):
-    #     claim = super(ClaimForm, self).save(commit=False)
-    #     if commit:
-    #         claim.save()
-    #         # self.save_m2m()  # doesnt work
-    #         for claim_coverage_type in self.claim_coverage_types:
-    #             try:
-    #                 exist = ClaimCoverage.objects.get(
-    #                     claim=claim,
-    #                     coverage_type=claim_coverage_type.coverage_type)
-    #                 exist.amount_claimed = claim_coverage_type.amount_claimed
-    #                 exist.expected_back = claim_coverage_type.expected_back
-    #                 exist.save()
-    #             except:
-    #                 claim_coverage_type.claim = claim
-    #                 claim_coverage_type.save()
-    #         for claim_item in self.claim_items:
-    #             try:
-    #                 exist = ClaimItem.objects.get(
-    #                     claim=claim, item=claim_item.item)
-    #                 exist.quantity = claim_item.quantity
-    #                 exist.save()
-    #             except:
-    #                 claim_item.claim = claim
-    #                 claim_item.save()
-    #         # for coverage_type in self.cleaned_data['coverage_types']:
-    #         #     coverage_type.save()
-    #     return claim
 
     class Meta:
         model = Claim
@@ -515,3 +343,49 @@ def minimum_nestedformset_factory(
         )
 
     return NestedFormSet  # Is class, not instance
+
+
+class ReferralForm(forms.ModelForm):
+
+    class EmptyClaimsQuerySet(Exception):
+        pass
+
+    class Meta:
+        model = clients_models.Referral
+        exclude = ('client',)
+        widgets = {'claims': forms.CheckboxSelectMultiple}
+
+    def __init__(self, client, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['claims'].label_from_instance = (
+            lambda obj:
+                "{} - {}".format(
+                    obj.patient,
+                    defaultfilters.date(obj.submitted_datetime,
+                                        "N j, Y, P")
+                )
+        )
+
+        claims_queryset = None
+        for referred in client.person_ptr.referred_by.select_related(
+                    'person_ptr', 'referred_by'
+                ).all():
+            claims = referred.claim_set.filter(
+                claimcoverage__actual_paid_date__isnull=False
+            )
+            if claims_queryset:
+                claims_queryset = claims_queryset | claims
+            else:
+                claims_queryset = claims
+
+        claims = clients_models.Referral.objects.prefetch_related(
+            'claims'
+        ).filter(client=client).values_list('claims', flat=True)
+        if claims_queryset:
+            claims_queryset = claims_queryset.exclude(id__in=claims)
+
+        if not claims_queryset:
+            raise self.EmptyClaimsQuerySet
+
+        self.fields['claims'].queryset = claims_queryset
