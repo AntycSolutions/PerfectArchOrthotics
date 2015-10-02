bba0135598c251f91e51783a5f24c41fb935ce5f
Solanar <and_555@hotmail.com>
2015-10-02 17:14:42 -0600

    urls for overdue claims pdf

--- clients/urls.py
+++ clients/urls.py
@@ -19,6 +19,9 @@ from clients.views import dependent
 
 report_patterns = patterns(
     '',
+    url(r'^overdue_claims_report/$',
+        login_required(statistics.overdue_claims_report),
+        name='overdue_claims_report'),
     url(r'^old_ordered_date_orders_report/$',
         login_required(statistics.old_ordered_date_orders_report),
         name='old_ordered_date_orders_report'),
