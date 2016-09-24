2b8b45018e7acab3c4c6764f9250ed8b2f71fca1
Solanar <and_555@hotmail.com>
2016-09-24 16:11:11 -0600

    add venv support to backup, update reqs
    - add comments
    - formatting
    - add fail branch on missing settings decision file
    - support virtual environment based setups
    - add further explanation for required bpython change
    - move pyreadline to windows.txt requirements

--- backup.sh
+++ backup.sh
@@ -5,26 +5,43 @@
 #  tar -xzf <file> to un-tar/gzip
 
 filename="default"
-now=$(date +"%Y_%m_%d")
+
+now=$(date +"%Y_%m_%d")  # year_month_day
+
+# cd into directory of this file
 dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cd $dir
 
-if [ -e "../devl" ]
-then
+# use settings decision file to name backup
+if [ -e "../devl" ]; then
     filename="devl"
-elif [ -e "../test" ]
-then
+elif [ -e "../test" ]; then
     filename="test"
-elif [ -e "../prod" ]
-then
+elif [ -e "../prod" ]; then
     filename="prod"
+else
+    echo 'settings decision file not found'
+    exit 1
 fi
 
 idx="$filename"_"$now"
 
+venv='../venv_perfect_arch'
+# check if we need to activate the virtual environment
+if [ -d "$venv" ]; then
+    source activate.sh
+fi
+
 # exclude contenttypes as they are created during migrate
 python3 manage.py dumpdata -e contenttypes > "$idx".json
+
+# deactivate the virtual environment
+if [ -d "$venv" ]; then
+    deactivate
+fi
+
 tar -czf ../backups/"$idx".tar.gz "$idx".json
 # Media folder is big, so overwrite it
 tar -czf ../backups/media_"$filename".tar.gz "media/"
+
 rm "$idx".json
