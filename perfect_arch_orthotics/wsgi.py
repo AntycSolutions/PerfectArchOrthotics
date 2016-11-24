82f8c624c4e18eb7dc7a236f77b207b2aab68b49
Solanar <and_555@hotmail.com>
2016-11-24 16:03:47 -0700

    appending project to sys.path required in prod

--- perfect_arch_orthotics/wsgi.py
+++ perfect_arch_orthotics/wsgi.py
@@ -4,12 +4,16 @@ WSGI config for perfect_arch_orthotics project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 """
 
+import sys
 from os import path, environ
 
 from django.core.wsgi import get_wsgi_application
 
 
 BASE_DIR = path.dirname(path.dirname(path.abspath(__file__)))
+
+# This is required in prod
+sys.path.append(BASE_DIR)
 
 environ.setdefault("DJANGO_SETTINGS_MODULE", "perfect_arch_orthotics.settings")
 
