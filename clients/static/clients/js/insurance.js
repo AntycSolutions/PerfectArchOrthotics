41ecedf7ec3ae84c88c5d7e23d1920041cf053bd
Solanar <and_555@hotmail.com>
2016-11-24 19:57:41 -0700

    formset_add_form_callback is now namespaced in window var generic_template

--- clients/static/clients/js/insurance.js
+++ clients/static/clients/js/insurance.js
@@ -1,25 +1,28 @@
+(function(generic_template) {
 
-function display_period_date(period_value, form, form_count) {
-    var period_date = form.find(
-        '#id_coverage_set-' + form_count + '-period_date'
-    );
+    function display_period_date(period_value, form, form_count) {
+        var period_date = form.find(
+            '#id_coverage_set-' + form_count + '-period_date'
+        );
 
-    // console.log(period_value, form, form_count, period_date);
-    if (period_value == '1') {
-        // parent is col parent is form-group
-        period_date.parent().parent().show();
+        // console.log(period_value, form, form_count, period_date);
+        if (period_value == '1') {
+            // parent is col parent is form-group
+            period_date.parent().parent().show();
+        }
+        else {
+            period_date.parent().parent().hide();
+        }
     }
-    else {
-        period_date.parent().parent().hide();
+
+    generic_template.formset_add_form_callback = function(form, form_count) {
+        var period = form.find('#id_coverage_set-' + form_count + '-period');
+
+        display_period_date(period.val(), form, form_count);
+
+        period.change(function() {
+            display_period_date($(this).val(), form, form_count);
+        });
     }
-}
 
-function formset_add_form_callback(form, form_count) {
-    var period = form.find('#id_coverage_set-' + form_count + '-period');
-
-    display_period_date(period.val(), form, form_count);
-
-    period.change(function() {
-        display_period_date($(this).val(), form, form_count);
-    });
-}
+}(window.generic_template = window.generic_template || {}));
