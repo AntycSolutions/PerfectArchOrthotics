7ffe60c89aef93bd1c128edf5592d761ad8d06cc
Solanar <and_555@hotmail.com>
2016-11-24 19:56:40 -0700

    don't need to use form_callback

--- inventory/static/inventory/js/coverage_order.js
+++ inventory/static/inventory/js/coverage_order.js
@@ -11,13 +11,12 @@ function display_claim(order_type_value, claim, form) {
     }
 }
 
-function form_callback(form) {
-    var order_type = form.find('#id_order_type');
-    var claim = form.find('#id_claim');
+var form = $('#create_coverage-order_form');
+var order_type = form.find('#id_order_type');
+var claim = form.find('#id_claim');
 
-    display_claim(order_type.val(), claim, form);
+display_claim(order_type.val(), claim, form);
 
-    order_type.change(function() {
-        display_claim($(this).val(), claim, form);
-    });
-}
+order_type.change(function() {
+    display_claim($(this).val(), claim, form);
+});
