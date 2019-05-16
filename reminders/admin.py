7c11773f7f46afc0e3e24ebb82c8fbf6854c8ce1
Andrew Charles <andrew.charles@antyc.ca>
2019-05-16 11:26:17 -0600

    add BenefitsReminder to admin

--- reminders/admin.py
+++ reminders/admin.py
@@ -6,3 +6,4 @@ from . import models
 admin.site.register(models.UnpaidClaimReminder)
 admin.site.register(models.OrderArrivedReminder)
 admin.site.register(models.ClaimOrderReminder)
+admin.site.register(models.BenefitsReminder)
